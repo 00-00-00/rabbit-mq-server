5fd9c3cff932ed1f74509dc0ac9cbbc03f51eb67
00-00-00 <nexusa8@gmail.com>
2018-07-29 16:14:00 +0530

    First commit

--- src/main/java/com/ground0/chumbucket/MainVerticle.kt
+++ src/main/java/com/ground0/chumbucket/MainVerticle.kt
@@ -3,8 +3,15 @@ package com.ground0.chumbucket
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.ext.web.Router
+import com.rabbitmq.client.ConnectionFactory
+
+
 
 class MainVerticle : AbstractVerticle() {
+
+    companion object {
+        const val QUEUE_NAME = "test_queue"
+    }
 
     @Throws(Exception::class)
     override fun start(future: Future<Void>) {
@@ -15,6 +22,25 @@ class MainVerticle : AbstractVerticle() {
             it.response().putHeader("content-type", "text/html")
                 .end("<h1>FOO FIGHTERS!</h1>")
         }
+
+        router.route("/send").handler{
+            val factory = ConnectionFactory()
+            factory.host = "localhost"
+            val connection = factory.newConnection()
+            val channel = connection.createChannel()
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+            val message = "Hello World!"
+            channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
+            println(" [x] Sent '$message'")
+
+
+            channel.close()
+            connection.close()
+
+            it.response().putHeader("content-type", "text/html").end("<h1>Done</h1>")
+        }
+
         vertx.createHttpServer()
             .requestHandler { router.accept(it) }
             .listen(
